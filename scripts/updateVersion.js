fc17d8873e0ca449a728d269e3d546e77055b7aa
AXELzip <axellp0001@gmail.com>
2025-05-15 00:22:08 -0600

    :tada: B1 Estructuras y Conceptos Básicos

--- scripts/updateVersion.js
+++ scripts/updateVersion.js
@@ -1,2 +1,2 @@
 const footer = document.querySelector(".legendVersion");
-footer.textContent = "V10.0 Section ramificaiciones (Git), code styles, leyenda desde JS";
+footer.textContent = "VPY1.0 Inicial";
