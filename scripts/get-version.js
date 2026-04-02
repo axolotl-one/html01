6d939d9a331716997062f2b6c7bf53b13e347486
AXELzip <axellp0001@gmail.com>
2026-04-01 23:25:11 -0600

    :sparkles: V12.0.1 Add navbar mobile para PATRON CSS

--- scripts/get-version.js
+++ scripts/get-version.js
@@ -1,7 +1,7 @@
-document.getElementById("version-legend").innerHTML = "V12.0.0 Refactorización del Sitio";
-document.getElementById("version-commit").innerHTML = "Hola Mundo, este es mi (XLI) Cuadragésimo Primer Commit de mi REPO de Git!";
+document.getElementById("version-legend").innerHTML = "V12.0.1 NavBar Mobile para PATRON CSS";
+document.getElementById("version-commit").innerHTML = "Hola Mundo, este es mi (XLII) Cuadragésimo Segundo Commit de mi Repo en Git!";
 document.getElementById("version-readme").innerHTML = 
-    "<p>Index: Actualización de Enlaces, Maquetados y Estilos</p>" + 
+    "<p>PATRON CSS: Implementación de la Barra de Navegación Móvil</p>" + 
     "<p>Version: Información de la Versión visible desde cualquier ventana</p>" + 
-    "<p>Lanzamiento de Nuevis Contenidos: SQL, REACTJS, JS</p>" +
+    "<p>Lanzamiento de Nuevos Contenidos: SQL, REACTJS, JS</p>" +
     "<p>&copy 2026 - Axel López (@axolotl-one)</p>";
