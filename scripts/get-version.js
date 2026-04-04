28d65bb579e5b35547407a63f777e4ef560debd6
AXELzip <axellp0001@gmail.com>
2026-04-03 23:57:52 -0600

    :bug: V12.1.1 Corrección de font-size autozoom de safari

--- scripts/get-version.js
+++ scripts/get-version.js
@@ -1,7 +1,7 @@
-document.getElementById("version-legend").innerHTML = "V12.1.0 Empaquetado de Funciones de Carga JSON";
-document.getElementById("version-commit").innerHTML = "Hola Mundo, este es mi (XLIII) Cuadragésimo Tercer Commit de mi Repo en Git!";
+document.getElementById("version-legend").innerHTML = "V12.1.1 Corrección de font-size autozoom";
+document.getElementById("version-commit").innerHTML = "Hola Mundo, este es mi (XLIV) Cuadragésimo Cuarto Commit de mi Repo en Git!";
 document.getElementById("version-readme").innerHTML = 
     "<p>Creación de <code>async-json.js</code>: Empaquetado de Funciones de Carga JSON</p>" + 
     "<p>PATRON C#: Las funciones de Carga JSON ahora se importan desde <code>async-json.js</code></p>" + 
-    "<p>Refactorización del <code>estilo02.css</code>: Corrección de Especificidades y tipografía</p>" +
+    "<p>Refactorización del <code>estilo02.css</code>: Corrección de font-size autozoom de safari</p>" +
     "<p>&copy 2026 - Axel López (@axolotl-one)</p>";
